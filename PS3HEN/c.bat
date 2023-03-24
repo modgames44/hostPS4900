0014458c0d0876089845034d95708fa955f4ceb7
Modgames44 <96064159+modgames44@users.noreply.github.com>
2023-03-23 23:35:40 -0300

    Delete PS3HEN directory

--- PS3HEN/c.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-for %%A in (PS3HEN.BIN_CEX_4??) do (
-set a=%%A
-set b=!a:~16,3!
-xcopy /Y PS3HEN.BIN_CEX_4!b! C:\PS3HEN\Make_PKG\4.!b!\dev_rewrite\hen\PS3HEN.BIN
-)
